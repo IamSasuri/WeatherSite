4ea156fc361cb9d3b02718303096fddec511e40f
Sasuri <97848497+IamSasuri@users.noreply.github.com>
2024-10-22 05:06:30 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import MainLayout from './Components/MainLayout';
+
+function App() {
+
+  return (
+    <div className="App text-6xl w-full flex justify-center items-center">
+      <MainLayout />
+    </div>
+  );
+}
+
+export default App;
